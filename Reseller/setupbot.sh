39f310db918296feba750b4dcddd187389493f11
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-11-04 14:47:24 +0800

    Update and rename Y to setupbot.sh

---
+++ Reseller/setupbot.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cd 
+mkdir -p sanbot
+cd sanbot
+
+# Install Modul
+python3 -m venv bot
+source bot/bin/activate
+pip install pyTelegramBotAPI
+deactivate
+
+
+
+cat <<EOL > /root/sanbot/run.sh
+#!/bin/bash
+source /root/sanbot/bot/bin/activate
+python3 /root/sanbot/menu.py
+EOL
+
+# Buat file service systemd
+cat <<EOF > /etc/systemd/system/sanbot.service
+[Unit]
+Description=San Bot Manager
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/bash /root/sanbot/run.sh
+WorkingDirectory=/root/sanbot/
+StandardOutput=inherit
+StandardError=inherit
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd dan mulai service
+systemctl daemon-reload
+systemctl enable sanbot
+systemctl start sanbot
+echo "Tekan Enter Untuk Menuju Menu Utama(↩️)"
+read -s
+menu
